2e8fd8f6c1460530925591c8e8a9630d1d28603c
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-09-07 22:47:07 +0300

    Delete my-first-io.js

--- my-first-io.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-let path = process.argv[2];
-try {
-    let text = fs.readFileSync(path, "utf-8");
-    console.log(text.split("\n").length - 1);    
-}catch (error) {
-    console.log("Unfortunately an error occured: "+error);
-}
