b968587fcbd91a26cbb5f3077dc73175b7717f1a
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-09-07 22:46:34 +0300

    Delete http-uppercaserer.js

--- http-uppercaserer.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer(function (req, res) {
-    if (req.method == "POST") {
-        req.on("data", function(data) {
-            res.write(String(data).toUpperCase());
-        })
-    }
-}).listen(parseInt(process.argv[2]));
